a6e987f93ed538a48516327ec0daa33007de6196
rsangijipa <doll.ricardoll@gmail.com>
2026-03-04 15:52:52 -0400

    fix(emerg): restore site settings and storage hostname

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
             { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
+            { protocol: 'https', hostname: 'storage.googleapis.com' },
             { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
             { protocol: 'https', hostname: 'img.youtube.com' },
             { protocol: 'https', hostname: 'i.ytimg.com' },
